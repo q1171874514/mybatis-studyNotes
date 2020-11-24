34991ac6ff048ed9488d82aa14d0a62937c1e36a
user <1171874514@qq.com>
2020-11-24 14:11:09 +0800

    创建项目和初始配置

---
+++ src/main/java/com/example/simplemybatis/SimpleMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplemybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMybatisApplication.class, args);
+    }
+
+}
